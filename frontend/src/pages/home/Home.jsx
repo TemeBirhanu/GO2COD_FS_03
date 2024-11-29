4d708c9da8b12e246adff1bdfc54011f5bff56cf
Temesgen Berhanu <temesgenberhanu674@gmail.com>
2024-11-29 00:03:13 -0800

    scroll

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-scroll bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 			<Sidebar className='sm:w-1/3 w-full' />
 			<MessageContainer className='sm:w-2/3 w-full' />
 		</div>
