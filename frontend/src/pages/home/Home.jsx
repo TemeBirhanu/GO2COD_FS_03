abc71976cbf3b4a1d3b525b30aa3fd6f3d90f82f
Temesgen Berhanu <temesgenberhanu674@gmail.com>
2024-11-29 03:02:10 -0800

    commit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,9 +3,9 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-scroll bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar className='sm:w-1/3 w-full' />
-			<MessageContainer className='sm:w-2/3 w-full' />
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar className='flex-1 sm:flex-none sm:w-1/3' />
+			<MessageContainer className='flex-1 sm:flex-none sm:w-2/3' />
 		</div>
 	);
 };
